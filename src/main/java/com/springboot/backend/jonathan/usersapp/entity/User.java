0409d2075533081640b6f17b264d1ceb00e25575
jonathan <johnkord17@gmail.com>
2025-05-06 01:20:44 -0500

    security JpaUserDetailsService

--- src/main/java/com/springboot/backend/jonathan/usersapp/entity/User.java
+++ src/main/java/com/springboot/backend/jonathan/usersapp/entity/User.java
@@ -1,7 +1,6 @@
 package com.springboot.backend.jonathan.usersapp.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
