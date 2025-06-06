36e3cec1b8ad6a925c886c84b24d9485782c7877
jonathan <johnkord17@gmail.com>
2025-06-05 20:55:04 -0500

    main limpio2

--- src/main/resources/db/migration/V2__create_roles_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS roles
-(
-    id int auto_increment primary key,
-    name varchar(50) not null,
-    user_id int not null,
-    foreign key (user_id) references users(id)
-);
